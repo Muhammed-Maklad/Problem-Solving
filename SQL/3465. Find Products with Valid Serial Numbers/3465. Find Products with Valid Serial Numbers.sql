6d7fd8350ad8a8001cc77b6ee49fa53a9a684594
Muhammed-Maklad <mmaklad@std.mans.edu.eg>
2026-08-20 13:09:16 +0300

    add a problem regex

--- SQL/3465. Find Products with Valid Serial Numbers/3465. Find Products with Valid Serial Numbers.sql
+++ SQL/3465. Find Products with Valid Serial Numbers/3465. Find Products with Valid Serial Numbers.sql
@@ -1,7 +1,6 @@
 SELECT *
 FROM products
-WHERE PATINDEX(
-    '%SN[0-9][0-9][0-9][0-9]-[0-9][0-9][0-9][0-9][^0-9]%',
-    description + ' '
-) > 0
+WHERE CONCAT(' ',description,' ')
+      LIKE '%[^A-Za-z0-9]SN[0-9][0-9][0-9][0-9]-[0-9][0-9][0-9][0-9][^A-Za-z0-9]%'  
+      COLLATE Latin1_General_100_BIN2
 ORDER BY product_id;
