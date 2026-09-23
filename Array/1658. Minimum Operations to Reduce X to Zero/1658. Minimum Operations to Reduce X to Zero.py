ed9f33cecb7a9fc2b1d4a98792d642b80815bb6f
Muhammed-Maklad <mmaklad@std.mans.edu.eg>
2026-09-23 14:10:43 +0300

    Day 23   Problem BS  problem

--- Array/1658. Minimum Operations to Reduce X to Zero/1658. Minimum Operations to Reduce X to Zero.py
+++ Array/1658. Minimum Operations to Reduce X to Zero/1658. Minimum Operations to Reduce X to Zero.py
@@ -5,18 +5,28 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
-        k = sum(nums) - x 
-        res = 0 
-        current_sum = 0 
-        left = 0
-        for right, num in enumerate(nums):
 
+        total = sum(nums)
+
+        if x > total:
+            return -1
+
+        if x == total:
+            return len(nums)
+
+        k = total - x
+        res = 0
+        current_sum = 0
+        left = 0
+
+        for right, num in enumerate(nums):
             current_sum += num
 
             while current_sum > k:
                 current_sum -= nums[left]
-                left += 1 
-            if current_sum == k:
-                res = max(res , right - left + 1)
+                left += 1
 
-        return(-1 if res <= 0 else len(nums) - res)
+            if current_sum == k:
+                res = max(res, right - left + 1)
+
+        return -1 if res == 0 else len(nums) - res
