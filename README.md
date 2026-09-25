9c1bbeb1868b29344cf662828f8084effd4b3709
Muhammed-Maklad <mmaklad@std.mans.edu.eg>
2026-09-25 17:00:47 +0300

    MAth\  problem

--- README.md
+++ README.md
@@ -437,7 +437,8 @@ This repository is a **production-ready resource** for software engineers prepar
 | 11 | [Add Two Integers](./Math/2235.%20Add%20Two%20Integers/Readme.MD) |![Easy](https://img.shields.io/badge/-Easy-5cb85c)  | 2235 | SUm |
 | 12 | [ Check if Number is a Sum of Powers of Three](./Math/1780.%20Check%20if%20Number%20is%20a%20Sum%20of%20Powers%20of%20Three/Readme.MD) |![Medium](https://img.shields.io/badge/-Medium-f0ad4e)| 1780 | Rule |
 | 13 | [Count Sorted Vowel Strings](./Math/1641.%20Count%20Sorted%20Vowel%20Strings/Readme.MD) |![Medium](https://img.shields.io/badge/-Medium-f0ad4e)| 1641 | combo |
-| 14 |
+| 14 | [Number of Steps to Reduce a Number to Zero](./Math/1342.%20Number%20of%20Steps%20to%20Reduce%20a%20Number%20to%20Zero/Readme.MD) |![Easy](https://img.shields.io/badge/-Easy-5cb85c)| 1342 | Reminder |
+| 15 | 
 
 
 </details>
--- test.py
+++ test.py
@@ -1,20 +1,92 @@
-class Solution(object):
-    def smallestIndex(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        # for index, num in enumerate(nums):
-        #     if index == sum(int(x) for x in str(num)):
-        #         return index
+# expression = "{a,{b,c}}"
 
-        # return -1
+# bracket = 0
 
-        for index , num in enumerate(nums):
-            total = 0
-            while num :
-                total += num % 10
-                num /= 10
-            if total == index :
-                return index
-        return -1 
+# res = set()
+# union = set()
+# current = ""
+
+# for x in expression:
+
+#     if x == "{":
+#         bracket += 1
+
+#     elif x == "}":
+#         if current:
+#             union.add(current)
+#             current = ""
+
+#         bracket -= 1
+
+#     if x.isalpha():
+#         current += x
+
+#     if x == "," and bracket == 1:
+#         union.add(current)
+#         current = ""
+
+
+# def concatenate(A, B):
+#     result = set()
+
+#     for a in A:
+#         for b in B:
+#             result.add(a + b)
+
+#     return result
+# def parse(expression):
+
+#     result = set()
+#     current = ""
+
+#     i = 0
+
+#     while i < len(expression):
+
+#         if expression[i].isalpha():
+#             current += expression[i]
+
+#         elif expression[i] == ",":
+#             result.add(current)
+#             current = ""
+
+#         elif expression[i] == "{":
+
+#             bracket = 1
+#             j = i + 1
+
+#             while bracket > 0:
+#                 if expression[j] == "{":
+#                     bracket += 1
+#                 elif expression[j] == "}":
+#                     bracket -= 1
+
+#                 j += 1
+
+#             inner_expression = expression[i + 1:j - 1]
+
+#             inner_result = parse(inner_expression)
+
+#             # لسه هنا محتاجين نقرر:
+#             # Union ولا Concatenation؟
+
+#         i += 1
+
+#     if current:
+#         result.add(current)
+
+#     return result
+
+
+
+num = 123
+step  = 0
+
+while num:
+    if num % 2 == 0 :
+        step +=1 
+        num /= 2 
+    else:
+        step +=1 
+        num -= 1
+print(step)
